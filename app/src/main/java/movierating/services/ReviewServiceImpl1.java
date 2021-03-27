fcca9f11567d3f0d09db7f02212584f994b8e79f
Niyathi3011 <alluniyathisri@gmail.com>
2021-03-27 19:16:53 +0530

    added null pointer exception for users and movies

--- app/src/main/java/movierating/services/ReviewServiceImpl1.java
+++ app/src/main/java/movierating/services/ReviewServiceImpl1.java
@@ -20,18 +20,22 @@ public class ReviewServiceImpl1 implements ReviewService {
     @Override
     public void addReview(String userName, String movieName, int rating) {
 
-        User user = this.userRepository.getUsers().get(userName);
-        if (user == null) {
+        User user;
+        if (this.userRepository.getUsers() != null && this.userRepository.getUsers().containsKey(userName)) {
+            user = this.userRepository.getUsers().get(userName);
+        } else {
             System.out.println("He is not a user of this Service");
             return;
         }
         Review review = new Review(user, rating);
-        Movie movie = this.movieRepository.getMovies().get(movieName);
-        if (movie == null) {
+
+        Movie movie;
+        if (movieRepository.getMovies() != null && movieRepository.getMovies().containsKey(movieName))
+            movie = this.movieRepository.getMovies().get(movieName);
+        else {
             System.out.println("Movie is not released yet");
             return;
         }
-
         movie.getReviewList().add(review);
         user.incrementTotalReviewsAndPromoteUserType();
 

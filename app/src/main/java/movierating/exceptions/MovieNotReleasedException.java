4e40bd72c02846fd57fe5032905595dc048d9fdb
Niyathi3011 <alluniyathisri@gmail.com>
2021-04-03 11:06:52 +0530

    Exceptions are added

--- app/src/main/java/movierating/exceptions/MovieNotReleasedException.java
+++ app/src/main/java/movierating/exceptions/MovieNotReleasedException.java
@@ -2,6 +2,6 @@ package movierating.exceptions;
 
 public class MovieNotReleasedException extends Exception {
     public MovieNotReleasedException(String movieName) {
-        super("Movie is not released yet"+movieName);
+        super(movieName + " is not released yet");
     }
 }
